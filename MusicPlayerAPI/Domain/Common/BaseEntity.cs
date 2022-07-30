ff2f2fa29d8bd7e16998d13c5ee3fd11202c4a82
Debanjan Paul <debanjanpaul10@gmail.com>
2022-07-30 13:19:45 +0530

    added entities and overall project structure

---
+++ MusicPlayerAPI/Domain/Common/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿// ***********************************************
+//	<copyright file="BaseEntity.cs" company="Personal">
+//		Copyright (c) 2022 Personal
+//	</copyright>
+// ***********************************************
+
+namespace Domain.Common
+{
+	using System.ComponentModel.DataAnnotations;
+
+	/// <summary>
+	/// Base Entity containing the Id that is common for all entities
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public abstract class BaseEntity<T>
+	{
+		/// <summary>
+		/// Id field
+		/// </summary>
+		[Required]
+		public virtual T Id { get; set; }
+	}
+}
---
+++ MusicPlayerAPI/Domain/Master/User.cs
@@ -0,0 +1,29 @@
+﻿// ***********************************************
+//	<copyright file="User.cs" company="Personal">
+//		Copyright (c) 2022 Personal
+//	</copyright>
+// ***********************************************
+
+namespace Domain.Master
+{
+	using System.ComponentModel.DataAnnotations;
+	using Domain.Common;
+	
+	/// <summary>
+	/// User entity class
+	/// </summary>
+	public class User : BaseEntity<int>
+	{
+		/// <summary>
+		/// User Email Id
+		/// </summary>
+		[Required]
+		public string Email { get; set; }
+
+		/// <summary>
+		/// User's name
+		/// </summary>
+		[Required]
+		public string Name { get; set; }
+	}
+}
